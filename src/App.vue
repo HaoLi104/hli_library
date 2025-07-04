b587403ccc278da2861b57e068182576a2165096
haoli104 <hlii0301@student.monash.edu>
2025-07-04 20:30:46 +0800

    Initial commit with all lab activities

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import Json from './components/JSON.vue'
+</script>
+
+<template>
+  <main>
+    <Json />
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 800px;
+  margin: 0 auto;
+  font-family: sans-serif;
+}
+</style>
